535791409456e29be6bb793e3e19836d665be9e4
itqanpos <mohamedalsonbaty673@gmail.com>
2026-04-15 02:03:28 +0200

    Update Supabase configuration with new values

--- js/supabase-config.js
+++ js/supabase-config.js
@@ -1,6 +1,7 @@
+// js/supabase-config.js
+const SUPABASE_URL = supabase link --project-ref emvqitmpdkkuyjzegyxf // استبدل بالرابط الخاص بك
+const SUPABASE_ANON_KEY =  supabase migration new new-migration        // استبدل بالمفتاح العام
 
-supabase link --project-ref emvqitmpdkkuyjzegyxf
-sb_publishable_rVcVXt2iVM80-bU6ZsxqsA_DPJQm4lN
 const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 window.supabase = supabase;
 console.log('✅ Supabase initialized');
